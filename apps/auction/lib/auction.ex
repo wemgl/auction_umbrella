a1d0a32307f1c8598cf93d86689ec52a800552b2
Wembley Leach <wembley.gl@gmail.com>
2019-06-29 16:23:26 -0400

    Initial commit

---
+++ apps/auction/lib/auction.ex
@@ -0,0 +1,21 @@
+defmodule Auction do
+  @moduledoc """
+  Documentation for Auction.
+  """
+
+  alias Auction.{FakeRepo, Item}
+
+  @repo FakeRepo
+
+  def list_items do
+    @repo.all(Item)
+  end
+
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
+  end
+end
