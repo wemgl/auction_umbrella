9e9d8eeaaaaf5235341eac0260bfb2d1b805483c
Wembley G. Leach, Jr <wembley.gl@gmail.com>
2019-06-30 12:09:44 -0400

    Add support for updating auction items

--- apps/auction/lib/auction/item.ex
+++ apps/auction/lib/auction/item.ex
@@ -1,5 +1,6 @@
 defmodule Auction.Item do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "items" do
     field :title, :string
@@ -7,4 +8,21 @@ defmodule Auction.Item do
     field :ends_at, :utc_datetime
     timestamps()
   end
+
+  def changeset(item, params \\ %{}) do
+    item
+    |> cast(params, [:title, :description, :ends_at])
+    |> validate_required(:title, message: "auction items need a title")
+    |> validate_length(:title, min: 3)
+    |> validate_length(:description, max: 200)
+    |> validate_change(:ends_at, &validate/2)
+  end
+
+  defp validate(:ends_at, ends_at_date) do
+    case DateTime.compare(ends_at_date, DateTime.utc_now()) do
+      :lt -> [ends_at: "auction ending dates must be in the future"]
+      _ -> []
+    end
+  end
+
 end
